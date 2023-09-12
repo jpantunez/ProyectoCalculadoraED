0a0a3b50cc1f04079839844acf76a068c4daf6d4
Luis Cuellar <luisenriquesanchez2004@hotmail.com>
2023-09-12 16:41:35 -0600

    Update ProyectoCalculadoraED.java

--- calculadora/ProyectoCalculadoraED.java
+++ calculadora/ProyectoCalculadoraED.java
@@ -3,6 +3,7 @@ package proyectocalculadoraed;
 public class ProyectoCalculadoraED {
 
     public static void main(String[] args) {
-        
+        VistaCalculadora vista = new VistaCalculadora();
+        vista.setVisible(true);
     }
 }
