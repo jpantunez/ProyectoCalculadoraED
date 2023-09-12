ab93fe12c6b70db02dc3da38d4557d3ab0c8f72f
Luis Cuellar <luisenriquesanchez2004@hotmail.com>
2023-09-12 12:50:37 -0600

    Update PilaADT.java

--- calculadora/PilaADT.java
+++ calculadora/PilaADT.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package calculadora;
-/**
- *
- * @author joseantunez
- */
-public interface PilaADT<T> {
+package proyectocalculadoraed;
+
+public interface PilaADT <T>{
     public void push(T dato);
     public T pop();
     public boolean isEmpty();
     public T peek();
-    public boolean multiPop(int n);
+    public void multiPop(int n);
 }
-
