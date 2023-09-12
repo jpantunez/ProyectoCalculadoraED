a8e46eee3970e131227b6c9ca611e69c110c25a9
Luis Cuellar <luisenriquesanchez2004@hotmail.com>
2023-09-12 17:13:53 -0600

    Update and rename ejecutableCalculadora.java to EjecutableCalculadora.java

--- calculadora/EjecutableCalculadora.java
+++ calculadora/EjecutableCalculadora.java
@@ -1,6 +1,6 @@
 package proyectocalculadoraed;
 
-public class ejecutableCalculadora {
+public class EjecutableCalculadora {
 
     public static void main(String[] args) {
         VistaCalculadora vista = new VistaCalculadora();
