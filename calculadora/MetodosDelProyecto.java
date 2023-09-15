5f52aaa4c8770b43ae63cfce2b8e31872b1f6307
Luis Cuellar <luisenriquesanchez2004@hotmail.com>
2023-09-15 01:26:44 -0600

    Update MetodosDelProyecto.java

--- calculadora/MetodosDelProyecto.java
+++ calculadora/MetodosDelProyecto.java
@@ -7,21 +7,21 @@ public class MetodosDelProyecto {
 
     // 1) Verificar validez
     // Se verifica si es un operador o es un numero
-
     private static boolean esOperador(String caracter) { // Se introduce un caracter en forma de cadena
         char operador = caracter.charAt(0);
         boolean resp;
         resp = false;
 
         // Se plantean los casos de los operadores
-        if (operador == '+' || operador == '-' || operador == '*' || operador == '/' || operador == '^') {
+        if (operador == '+' || operador == '-' || operador == '*' || operador == '/' || operador == '^'){
             resp = true; // Si es alguno, la respuesta es verdadera
             return resp;
-        } else {
+        } 
+        else {
             return resp;
         }
     }
-
+    
     // Este metodo se introduce una cadena y trata de convertirla en numero, si lo
     // logra, boolean es true, si marca false, es porque no pudo convertirla
     private static boolean esNumero(String cadena) { // Se introduce una cadena
@@ -32,17 +32,16 @@ public class MetodosDelProyecto {
             numero = (Double.parseDouble(cadena));
             resp = true;
             return resp;
-        } catch (NumberFormatException e) {
+        }
+        catch (NumberFormatException e) {
             return resp;
         }
     }
-
+    
     // 2) Convertir de infija a postfija
-
     // Este metodo busca convertir de infija a postfija
     // El usuario ingresa su operacion de manera infija
     // Devuelve la operacion de manera postfija
-
     public static PilaADT<String> convierteInfijaPostfija(String textoCalcular) { // Devuelve pila de cadenas
         PilaA<Character> pila = new PilaA(); // Pila de caracteres
         PilaA<String> postfija = new PilaA(); // Pila de cadenas
@@ -52,9 +51,8 @@ public class MetodosDelProyecto {
 
         for (int i = 0; i < textoCalcular.length(); i++) { // Verificado con la longitud de la expresion introducida
             caracter = textoCalcular.charAt(i); //caracter en la pos i
-            // Checa si es un numero u operador+
-            if(i==1 && textoCalcular.charAt(i-1)=='-' && '-'==textoCalcular.charAt(i)
-                    ||caracter.equals('-') && textoCalcular.length()-1==i){
+            // Checa si es un numero u operador
+            if (i==1 && textoCalcular.charAt(i-1)=='-' && '-'==textoCalcular.charAt(i)||caracter.equals('-') && textoCalcular.length()-1==i){
                 VistaCalculadora.syntaxErrorOccurred = true;
             }
             if(((i==0 || textoCalcular.charAt(i-1)=='+' || textoCalcular.charAt(i-1)=='-'
@@ -64,13 +62,13 @@ public class MetodosDelProyecto {
             }
             else if (Character.isDigit(caracter)) { // Si es numero, se agrega directo a la pila
                 cadena = preparaCadena(textoCalcular, i); // Se utiliza un metodo aparte para generar cadena
-                if(verificacion==1){
+                if (verificacion==1){
                     postfija.push("0");
                     pila.push('-');
                     postfija.push(cadena);
                     verificacion = 0;
                 }
-                else{
+                else {
                     postfija.push(cadena); // Se agrega a la pila
                 }
                 i = i + cadena.length() - 1; // Se incrementa el iterador
@@ -80,17 +78,20 @@ public class MetodosDelProyecto {
                 if (verificacion==1){
                     VistaCalculadora.syntaxErrorOccurred = true;
                     verificacion=0;
-                }else{
+                }
+                else{
                     if (caracter == '(') {
                         pila.push('(');
-                    } else { // Si es parentesis que cierra
+                    }
+                    else { // Si es parentesis que cierra
                         if (caracter == ')') {
                             while (!pila.isEmpty() && pila.peek() != '(') { // Se ejecuta mientras que la pila no este vacia y el tope sea distinto de parentesis que abre
                                 postfija.push(pila.pop() + "");
                             }
                             if (pila.peek() == '(')
                                 pila.pop();
-                        } else { // Se utiliza jerarquia (potencia, multiplicacion y division, suma y resta)
+                        }
+                        else { // Se utiliza jerarquia (potencia, multiplicacion y division, suma y resta)
                             // Si es potencia
                             if (caracter == '^') {
                                 if (!pila.isEmpty())
@@ -98,14 +99,16 @@ public class MetodosDelProyecto {
                                             && pila.peek() != '+' && pila.peek() != '-')
                                         postfija.push(pila.pop() + "");
                                 pila.push(caracter);
-                            } else { // Si es multiplicacion o division
+                            }
+                            else { // Si es multiplicacion o division
                                 if (caracter == '/' || caracter == '*') {
                                     if (!pila.isEmpty())
                                         while (!pila.isEmpty() && pila.peek() != '(' && pila.peek() != '+'
                                                 && pila.peek() != '-')
                                             postfija.push(pila.pop() + "");
                                     pila.push(caracter);
-                                } else { // Si es suma o resta
+                                }
+                                else { // Si es suma o resta
                                     if (caracter == '+' || caracter == '-') {
                                         if (!pila.isEmpty())
                                             while (!pila.isEmpty() && pila.peek() != '(')
@@ -120,14 +123,16 @@ public class MetodosDelProyecto {
             }
         }
         while (!pila.isEmpty()) { // Verifica parentesis
-            if (pila.peek() == '(')
+            if (pila.peek() == '('){
                 pila.pop();
-            else
+            }
+            else {
                 postfija.push(pila.pop() + "");
+            }
         }
         return postfija;
     }
-
+    
     // Metodo para generar cadena
     private static String preparaCadena(String textoCalcular, int i) { // Recibe cadena y entero para ver donde empieza
         String cadena = textoCalcular.charAt(i) + "";
@@ -152,19 +157,19 @@ public class MetodosDelProyecto {
     }
 
     // 3) Realizar operaciones, desde su forma postfija
-
     public static double calculoPostfija(PilaADT<String> pila) { // Recibe pila
         double resultado = 0, auxiliar1, auxiliar2, res;
         char operadorChar;
         String operadorString; // Operador
-
         PilaA<Double> aux = new PilaA(); // Crear pila auxiliar de double
+        
         inviertePila(pila); // Metodo aparte para invertir pila
         while (!pila.isEmpty()) { // Mientras no este vacio
             if (esNumero(pila.peek())) { // Llamar al metodo esNumero
                 aux.push(Double.valueOf(pila.pop())); // Se extrae y se convierte en double, agregandose a la pila auxiliar
-            } else // Caso en que sea operador
-            if (esOperador(pila.peek())) { // Checar con el tope de la pila
+            }
+            // Caso en que sea operador
+            else if (esOperador(pila.peek())) { // Checar con el tope de la pila
                 if (!aux.isEmpty()) {
                     auxiliar2 = aux.pop();
                     auxiliar1 = aux.pop();
@@ -202,11 +207,13 @@ public class MetodosDelProyecto {
                 }
             }
         }
-        if (!aux.isEmpty()) // Si la pila no esta vacia
+        if (!aux.isEmpty()) { // Si la pila no esta vacia
             resultado = aux.pop(); // Extraer valor superior
+        }    
         return resultado; // Se regresa el valor acumulado de la pila
     }
     
+    // main para probar el proyecto desde la terminal
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         String expresion;
